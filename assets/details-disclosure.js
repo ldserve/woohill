0b29cadded3cd425a12fb7c56da95661725beb8c
jiang <1352234513@qq.com>
2022-05-04 15:00:44 +0800

    搜索栏修改2

--- assets/details-disclosure.js
+++ assets/details-disclosure.js
@@ -1,9 +1,11 @@
 class DetailsDisclosure extends HTMLElement {
   constructor() {
     super();
+    this.isclose=false
     this.mainDetailsToggle = this.querySelector('details');
-
     this.mainDetailsToggle.addEventListener('focusout', this.onFocusOut.bind(this));
+    this.mainDetailsToggle.addEventListener('mouseleave', this.mouseClose.bind(this));
+    this.mainDetailsToggle.addEventListener('mouseenter', this.mouseClose.bind(this));
   }
 
   onFocusOut() {
@@ -15,6 +17,13 @@ class DetailsDisclosure extends HTMLElement {
   close() {
     this.mainDetailsToggle.removeAttribute('open');
     this.mainDetailsToggle.querySelector('summary').setAttribute('aria-expanded', false);
+  }
+  mouseClose(e){
+    this.isclose=e.type==='mouseleave'
+    setTimeout(()=>{
+      if(this.isclose)this.close();
+    },200)
+    
   }
 }
 
